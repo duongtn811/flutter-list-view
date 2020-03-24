b649b32df6e4bb7f6968fa5d0496f34758959299
Duong Nguyen Van <duong.nguyen@grabtaxi.com>
2020-03-24 17:01:45 +0700

    todo list and add item

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/utils/dbhelper.dart';
+import 'package:flutter_app/model/todo.dart';
+import 'package:flutter_app/screens/todo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Todos',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(title: 'Todos'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+        ),
+        body: TodoList(),
+    );
+  }
+}
